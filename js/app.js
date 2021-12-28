20ad8fee45b70fff46ef460e899ab47eff697f7b
Your Name <you@example.com>
2021-12-28 17:46:15 +0300

    add 2

--- js/app.js
+++ js/app.js
@@ -13,32 +13,58 @@ function shuffle(array) {
 }
 
 // varible
-let duration=1000;
+let openCard = []
+const cards = document.querySelectorAll(".card")
+let isMaching =[]
+
+
+
 
 //functions
+function match(){
+    if(openCard[0].children[0].className === openCard[1].children[0].className){
+        
+        event.target.classList.add("match");
+        openCard[0].classList.add("match");
+        
+
+        //
+        openCard=[];
+    } else{
+        
+        setTimeout(()=>{
+            openCard[0].classList.remove("open");
+            openCard[1].classList.remove("open");
 
 
+            //
+            openCard=[];
+        },1000)    
+    }
+}
 // event listeners
-const cards = document.querySelectorAll(".card")
+
 for(let card of cards){
-    card.addEventListener("click",(event)=>{
-        event.target.classList.add("open");
-    })
+        card.addEventListener("click",(event)=>{
+
+            event.target.classList.add("open");
+
+            //add the card what I clickes on to the array named 'openCard'
+            openCard.push(event.target);
+            //
+            if(openCard.length == 2){
+                match();
+            } else{ stopPush;
+            }
+            
+        })
 }
 
-
-var arr[card1, card2];
-function match(card1 ,card2){
-    if(card1 === card2){
-        
-        console.log("hi");
-    } else{
-        cards.classList.remove("open");
+function stopPush(){
+    if(openCard > 2 ){
+        openCard.shift();
     }
-
-
-
-//const isMaching
+}
     
 
 
